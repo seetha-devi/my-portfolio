65ead737232d8602976ba9d50e37fb65f40d84ed
seetha-devi <seethadevi322001@gmail.com>
2024-08-11 18:59:13 +0530

    updated style for resume

--- components/shared/AppBanner.jsx
+++ components/shared/AppBanner.jsx
@@ -51,7 +51,7 @@ function AppBanner() {
 						download="seethadevi_resume.pdf"
 						href="/files/seethadevi_resume.pdf"
 						className="font-general-medium flex justify-center items-center w-36 sm:w-48 mt-12 mb-6 sm:mb-0 text-lg border border-indigo-200 dark:border-ternary-dark py-2.5 sm:py-3 shadow-lg rounded-lg bg-indigo-500 focus:ring-1 focus:ring-indigo-900 hover:bg-indigo-900 text-grey-900 hover:text-white duration-500"
-						aria-label="Download Resume"
+						aria-label="Downloads Resume"
 					>
 						<FiArrowDownCircle className="ml-0 sm:ml-1 mr-2 sm:mr-3 h-5 w-5 sn:w-6 sm:h-6 duration-100"></FiArrowDownCircle>
 						<span className="text-sm sm:text-lg duration-100">
